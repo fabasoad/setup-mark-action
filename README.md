d73995205d901252f1eb6f16d0de18590fcae5a4
Eugene <fabasoad@gmail.com>
2024-03-26 21:37:02 +0900

    Setup mark installation (#1)

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# setup-mark-action
-This GitHub action installs kovetskiy/mark CLI.
+# Setup mark action
+
+[![Stand With Ukraine](https://raw.githubusercontent.com/vshymanskyy/StandWithUkraine/main/badges/StandWithUkraine.svg)](https://stand-with-ukraine.pp.ua)
+![Release](https://img.shields.io/github/v/release/fabasoad/setup-mark-action?include_prereleases)
+![functional-tests](https://github.com/fabasoad/setup-mark-action/actions/workflows/functional-tests.yml/badge.svg)
+![security](https://github.com/fabasoad/setup-mark-action/actions/workflows/security.yml/badge.svg)
+![linting](https://github.com/fabasoad/setup-mark-action/actions/workflows/linting.yml/badge.svg)
+
+This action installs [mark](https://github.com/kovetskiy/mark) CLI tool.
+
+Supported OS: Linux and macOS.
+
+## Prerequisites
+
+The following tools have to be available on a runner prior using this GitHub
+action:
+
+- [curl](https://curl.se/)
+- [tar](https://www.gnu.org/software/tar/)
+
+## Inputs
+
+```yaml
+- uses: fabasoad/setup-mark-action@v0
+  with:
+    # (Optional) mark version. Defaults to "9.11.1".
+    version: "9.11.1"
+```
+
+## Outputs
+
+None.
---
+++ src/install-mark.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+mark_version="$1"
+
+# shellcheck disable=SC2039
+if command -v mark >/dev/null 2>&1; then
+  msg="$(mark --version) is already installed. Skipping installation."
+  printf "[setup-mark-action] %s level=info %s\n" "$(date +'%Y-%m-%d %T')" "${msg}"
+  exit 0
+fi
+
+url=""
+if [ "${RUNNER_OS}" = "macOS" ]; then
+  if [ "${RUNNER_ARCH}" = "X64" ]; then
+    url="https://github.com/kovetskiy/mark/releases/download/${mark_version}/mark_Darwin_x86_64.tar.gz"
+  elif [ "${RUNNER_ARCH}" = "ARM64" ]; then
+    url="https://github.com/kovetskiy/mark/releases/download/${mark_version}/mark_Darwin_arm64.tar.gz"
+  fi
+elif [ "${RUNNER_OS}" = "Linux" ]; then
+  if [ "${RUNNER_ARCH}" = "X64" ]; then
+    url="https://github.com/kovetskiy/mark/releases/download/${mark_version}/mark_Linux_x86_64.tar.gz"
+  elif [ "${RUNNER_ARCH}" = "ARM64" ]; then
+    url="https://github.com/kovetskiy/mark/releases/download/${mark_version}/mark_Linux_arm64.tar.gz"
+  fi
+fi
+
+if [ -z "${url}" ]; then
+  echo "::error title=OS is not supported::${RUNNER_OS} ${RUNNER_ARCH} is not supported"
+  exit 1
+fi
+
+bin_path="${RUNNER_TEMP}/bin"
+mkdir -p "${bin_path}"
+tar_path="${bin_path}/mark.tar.gz"
+curl -sL "${url}" -o "${tar_path}"
+tar -xf "${tar_path}" -C "${bin_path}"
+rm -f "${tar_path}"
+echo "${bin_path}" >> "$GITHUB_PATH"
