bbfbb2cac81da5010823d018b201313eb498ef6c
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2024-04-13 18:27:37 +0900

    Add sha256sum installation 2

--- src/install-mark.sh
+++ src/install-mark.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env sh
 
-SCRIPT_PATH=$(realpath "$0")
-SRC_PATH=$(dirname "$SCRIPT_PATH")
-
-. "${SRC_PATH}/install-sha256sum.sh"
-
 mark_version="$1"
 
 # shellcheck disable=SC2039
@@ -48,7 +43,14 @@ curl -sL "${url}" -o "${tar_path}"
 curl -sL "${url_prefix}/checksums.txt" -o "${checksums_path}"
 
 if ! command -v sha256sum >/dev/null 2>&1; then
-  install_sha256sum
+  echo "::group::Installing sha256sum"
+  if [ "${RUNNER_OS}" = "macOS" ]; then
+    brew install coreutils
+  else
+    msg="sha256sum was not installed on the current ${RUNNER_OS} machine"
+    printf "[setup-mark-action] %s level=warn %s\n" "$(date +'%Y-%m-%d %T')" "${msg}"
+  fi
+  echo "::endgroup::"
 fi
 
 if ! grep -qF "$(sha256sum "${tar_path}" | cut -d ' ' -f 1)" "${checksums_path}"; then
--- src/install-sha256sum.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-install_sha256sum() {
-  echo "::group::Installing sha256sum"
-  if [ "${RUNNER_OS}" = "macOS" ]; then
-    brew install coreutils
-  fi
-  echo "::endgroup::"
-}
