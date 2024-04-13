86256152f0d66963704bcf7dee26bde8ff59a8bf
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2024-04-13 21:03:36 +0900

    Bump pre-commit/pre-commit-hooks from 4.5.0 to 4.6.0

--- src/install-mark.sh
+++ src/install-mark.sh
@@ -42,17 +42,6 @@ checksums_path="${bin_path}/checksums.txt"
 curl -sL "${url}" -o "${tar_path}"
 curl -sL "${url_prefix}/checksums.txt" -o "${checksums_path}"
 
-#if ! command -v sha256sum >/dev/null 2>&1; then
-#  echo "::group::Installing sha256sum"
-#  if [ "${RUNNER_OS}" = "macOS" ]; then
-#    brew install coreutils
-#  else
-#    msg="sha256sum was not installed on the current ${RUNNER_OS} machine"
-#    printf "[setup-mark-action] %s level=warn %s\n" "$(date +'%Y-%m-%d %T')" "${msg}"
-#  fi
-#  echo "::endgroup::"
-#fi
-
 if ! grep -qF "$(shasum -a 256 "${tar_path}" | cut -d ' ' -f 1)" "${checksums_path}"; then
   echo "::error title=Checksum error::Checksum is different from the downloaded binary"
   exit 1
